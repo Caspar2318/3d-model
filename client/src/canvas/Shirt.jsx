8b0304e0574ca5d27ac712094826b5547b965473
Caspar2318 <108163945+Caspar2318@users.noreply.github.com>
2023-04-04 17:02:50 +1000

    update materials

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -9,6 +9,7 @@ import state from "../store";
 const Shirt = () => {
   const snap = useSnapshot(state);
   const { nodes, materials } = useGLTF("./shirt_baked.glb");
+  console.log(materials);
 
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
